6b81cb81ccc56523aee8ee98d128de908d84b670
tatianasnook <tatsnook@gmail.com>
2024-01-12 10:48:11 -0600

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -3,10 +3,19 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
 userSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model('User', userSchema);
